de4ac962f47edad7c9bdc54082fbde88872953a1
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-05 23:21:57 +0300

    feat: add parsing to DB

--- parsing_and_data/DB.py
+++ parsing_and_data/DB.py
@@ -1,5 +1,25 @@
 import psycopg2
+from psycopg2 import Error
 
-conn = psycopg2.connect(dbname='postgres', user='postgres', password='1234567890', host='192.168.194.89')
+def tobd(list):
+    try:
+        connection = psycopg2.connect(database='',
+                                    user='',
+                                    password='',
+                                    host='', 
+                                    port = 0)  
+        
+        cursor = connection.cursor()
 
-conn.close()
+        insert_query = f"""INSERT INTO datasets (address_name, price, square_price, floors, rooms, area, local, type_home, remont, balcon) VALUES {list}"""
+
+        cursor.execute(insert_query)
+        connection.commit()
+    except (Exception, Error) as error:
+        return 1
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+    return 0
