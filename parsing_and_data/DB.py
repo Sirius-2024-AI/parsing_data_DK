9c997793580eb2174fdf4313e047c904627ffea7
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-06 12:20:15 +0300

    fix: fix database fn

--- parsing_and_data/DB.py
+++ parsing_and_data/DB.py
@@ -1,25 +1,28 @@
 import psycopg2
 from psycopg2 import Error
+from CONFIG import *
 
 def tobd(list):
     try:
-        connection = psycopg2.connect(database='',
-                                    user='',
-                                    password='',
-                                    host='', 
-                                    port = 0)  
-        
+        connection = psycopg2.connect(database=database,
+                                        user=user,
+                                        password=password,
+                                        host=host, 
+                                        port = port)  
+            
         cursor = connection.cursor()
 
-        insert_query = f"""INSERT INTO datasets (address_name, price, square_price, floors, rooms, area, local, type_home, remont, balcon) VALUES {list}"""
+        insert_query = f"""INSERT INTO datasets (address_name, price, square_price, floors, rooms, area, city, type_home, remont, balcon, url) VALUES {list}"""
 
         cursor.execute(insert_query)
         connection.commit()
+        #print("y")
     except (Exception, Error) as error:
-        return 1
+        print(error)
     finally:
         if connection:
             cursor.close()
             connection.close()
-
+            #print("y")
+    #print("y")
     return 0
