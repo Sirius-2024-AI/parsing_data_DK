56868f3a84cce3ab8bbabf05f54c0b6be3a2eb8b
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-07 23:43:23 +0300

    feat: catch all err

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ parser.add_argument('--city', help='txt file with list of many citys')
 parser.add_argument('--town', help='str city name')
 
 args = parser.parse_args()
-
-if args.city is None:
-    main_parser_fn(get_guid_of_regione(args.town), str(args.dbname), str(args.dbuser), str(args.dbpaswd), str(args.dbhost), int(args.dbport), str(args.tablename))
-    print("Finish parsing: {args.town}")
-else:
-    f = open(args.city, 'r', encoding="utf-8")
-    f = list(f.read().split(","))
-    for i in f:
-        print(i)
-        main_parser_fn(get_guid_of_regione(i), str(args.dbname), str(args.dbuser), str(args.dbpaswd), str(args.dbhost), int(args.dbport), str(args.tablename))
-        print("Finish parsing: {i}")
-
+try:
+    if args.city is None:
+        main_parser_fn(get_guid_of_regione(args.town), str(args.dbname), str(args.dbuser), str(args.dbpaswd), str(args.dbhost), int(args.dbport), str(args.tablename))
+        print("Finish parsing: {args.town}")
+    else:
+        f = open(args.city, 'r', encoding="utf-8")
+        f = list(f.read().split(","))
+        for i in f:
+            print(i)
+            main_parser_fn(get_guid_of_regione(i), str(args.dbname), str(args.dbuser), str(args.dbpaswd), str(args.dbhost), int(args.dbport), str(args.tablename))
+            print("Finish parsing: {i}")
+except Exception as e:
+    print(e)
 
