79371e50fb0d25766f78a3319075a255ab27d430
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-07 23:48:00 +0300

    fix: random exits due to errors

--- parsing_and_data/paring_data/all_def.py
+++ parsing_and_data/paring_data/all_def.py
@@ -17,20 +17,26 @@ class DomClickApi:
         self.get("https://ipoteka.domclick.ru/mobile/v1/feature_toggles")
 
     def get(self, url, **kwargs):
-        self.__update_headers(url, **kwargs)
-        result = self.session.get(url, **kwargs)
-        #print(self.session.cookies.get_dict())
-        return result
+        try:
+            self.__update_headers(url, **kwargs)
+            result = self.session.get(url, **kwargs)
+            #print(self.session.cookies.get_dict())
+            return result
+        except:
+            pass
 
     def __update_headers(self, url, **kwargs):
-        url = self.__get_prepared_url(url, **kwargs)
-        sault = "ad65f331b02b90d868cbdd660d82aba0"
-        timestamp = str(int(datetime.now().timestamp()))
-        encoded = (sault + url + timestamp).encode("UTF-8")
-        h = hashlib.md5(encoded).hexdigest()
-        self.session.headers.update({"Timestamp": timestamp,
-                                     "Hash": "v1:" + h,
-                                     })
+        try:
+            url = self.__get_prepared_url(url, **kwargs)
+            sault = "ad65f331b02b90d868cbdd660d82aba0"
+            timestamp = str(int(datetime.now().timestamp()))
+            encoded = (sault + url + timestamp).encode("UTF-8")
+            h = hashlib.md5(encoded).hexdigest()
+            self.session.headers.update({"Timestamp": timestamp,
+                                        "Hash": "v1:" + h,
+                                        })
+        except:
+            pass
 
     def __get_prepared_url(self, url, **kwargs):
         p = requests.models.PreparedRequest()
@@ -49,43 +55,43 @@ def get_guid_of_regione(regione) -> str:
 
 def parser(addresse, database, user, password, host, port, tablename, offers_url, count_url, dca, vid, rem, sd, room, balcon, typy):
     offset = 0
-    for year in range(2024, 1950, -1):
-        req = dca.get(count_url, params={
-            "address": addresse,
-            "deal_type": "sale",
-            "category": "living",
-            "window_view": vid,
-            "offer_type": [sd],
-            "renovation": rem,
-            "rooms": room,  
-            "build_year__gte": year,
-            "build_year__lte": year,
-            "wall_type": typy,
-            "balconies": balcon
-        })
-                                        
-        count_obj = json.loads(req.text)
-        total = count_obj["pagination"]["total"]
-        #print(count_obj['result'])
-        print(f"finishing searching total args for: {year}")
-        print(total)
-        for offset in range(0, total, 1):
-            try: 
+    try:
+        for year in range(2024, 1950, -1):
+            req = dca.get(count_url, params={
+                "address": addresse,
+                "deal_type": "sale",
+                "category": "living",
+                "window_view": vid,
+                "offer_type": [sd],
+                "renovation": rem,
+                "rooms": room,  
+                "build_year__gte": year,
+                "build_year__lte": year,
+                "wall_type": typy,
+                "balconies": balcon
+            })
+                                            
+            count_obj = json.loads(req.text)
+            total = count_obj["pagination"]["total"]
+            #print(count_obj['result'])
+            print(f"finishing searching total args for: {year}")
+            print(total)
+            for offset in range(0, total, 1):
                 res = dca.get(offers_url, params={
-                                                    "address": addresse,
-                                                    "deal_type": "sale",
-                                                    "category": "living",
-                                                    "offer_type": [sd],
-                                                    "window_view": vid,
-                                                    "offset": offset,
-                                                    "limit": 20,
-                                                    "rooms": room,
-                                                    "build_year__gte": year,
-                                                    "build_year__lte": year,
-                                                    "wall_type": typy,
-                                                    "renovation": rem,
-                                                    "balconies": balcon, 
-                                                })
+                                                        "address": addresse,
+                                                        "deal_type": "sale",
+                                                        "category": "living",
+                                                        "offer_type": [sd],
+                                                        "window_view": vid,
+                                                        "offset": offset,
+                                                        "limit": 20,
+                                                        "rooms": room,
+                                                        "build_year__gte": year,
+                                                        "build_year__lte": year,
+                                                        "wall_type": typy,
+                                                        "renovation": rem,
+                                                        "balconies": balcon, 
+                                                    })
                 print("RES:", res)
                 offers_obj = json.loads(res.text)
                 result_data = offers_obj["result"]
@@ -98,24 +104,24 @@ def parser(addresse, database, user, password, host, port, tablename, offers_url
                     object_info = item['object_info']
                     description = item['description']
                     row = (
-                            address['name'],
-                            price['price'],
-                            object_info['floor'],
-                            house['floors'],
-                            object_info['rooms'],
-                            object_info['area'],
-                            address['locality']['name'],
-                            sd,
-                            rem,
-                            balcon,
-                            address['guid'],
-                            vid,
-                            description,
-                            year,)
-                            #print(row)
+                                address['name'],
+                                price['price'],
+                                object_info['floor'],
+                                house['floors'],
+                                object_info['rooms'],
+                                object_info['area'],
+                                address['locality']['name'],
+                                sd,
+                                rem,
+                                balcon,
+                                address['guid'],
+                                vid,
+                                description,
+                                year,)
+                                #print(row)
                     tobd(row, database, user, password, host, port, tablename)                        
-            except:
-                print("error")
+    except:
+        print("error")
                 #continue
 
 def main_parser_fn(addresse, database, user, password, host, port, tablename):#, database, user, password, host, port):    
